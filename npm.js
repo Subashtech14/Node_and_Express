6b9a1a02fb0878801e50ab95e46a4426ba6e5c63
Subash <Subashtech14@gmail.com>
2023-06-01 17:53:48 +0530

    Modules adding

--- npm.js
+++ npm.js
@@ -5,3 +5,8 @@
 // global dependency - use it in any project
 // npm install -g <packageName>
 // npm uninstall -g <packageName>
+// npm update -g <packageName>
+// package.json - manifest file (stores important info about project/package)
+// manual approach (create package.json in the root, create properties etc)
+// npm init (step by step, press enter to skip)
+// npm init -y (everything default)
