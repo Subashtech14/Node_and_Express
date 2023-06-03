4d4757b3a2f7fcdc2fcc630a360ca209101a14f9
Subash <Subashtech14@gmail.com>
2023-06-03 09:41:03 +0530

    Morgan for Logging

--- 02-express-tutorial/basic/middleware.js
+++ 02-express-tutorial/basic/middleware.js
@@ -1,10 +1,13 @@
 //middleware are functions that excecutes during a request to the server
 const express = require('express');
 const app = express();
+const morgan =require('morgan'); 
 //req => middleware => response
 const logger=require('./logger');
+const authorize=require('./authorized');
 //app.use(logger); it will invoke the logger for every route
-app.use(logger)
+// app.use([authorize,logger])
+app.use(morgan('tiny'));
 //app.use('/api',logger); This will invoke the logger for every route after api
 app.get('/', (req, res) => {
   res.send('Home'); 
@@ -15,8 +18,9 @@ app.get('/about', (req, res) => {
 app.get('/api/products', (req, res) => {
     res.send('Products'); 
   })
-app.get('/api/results', (req, res) => {
-    res.send('Resulsts'); 
+app.get('/api/items', (req, res) => {
+  console.log(req.user);
+    res.send('Items'); 
   })
 app.get('*', (req, res) => {
   res.status('404').send('404 - Page Not Found');
