179b48dcbd3de97d40e42dd4a6c8bf42ddc7822d
Subash <Subashtech14@gmail.com>
2023-06-03 16:44:05 +0530

    Finsihed

--- 02-express-tutorial/basic/app.js
+++ 02-express-tutorial/basic/app.js
@@ -1,22 +1,31 @@
-const express=require('express');
-const app=express();
-app.get('/',(req,res)=>{
-  res.status().send('Hello World');  
+const express= require('express');
+const app= express();
+let {people}= require('./data');
+
+//static assets
+app.use(express.static('../methods-public'));
+//parse from data
+app.use(express.urlencoded({extended:false}));
+app.use(express.json());
+app.get('/api/people',(req,res)=>{
+    res.status(200).json({success:true,data:people});
 })
-app.get('/about',(req,res)=>{
-  res.status(200).send('About Page');  
+app.post('/api/people',(req,res)=>{
+    const {name} = req.body;
+    if(!name){
+        return res.send(404).json({success:false,message:'Please provide a name value'})
+    }
+    res.status(201).json({success:true,person:name});
 })
-app.all('*',(req,res)=>{
-  res.status(404).send('Page Not Found');
+app.post('/login',(req,res)=>{
+    const name= req.body.name;
+    if(name){
+    res.status(200).send(`Welcome ${name}`);
+    }
+    else{
+        res.status(400).send('Please Provide Crediantls');
+    }
 })
 app.listen(5000,()=>{
-    console.log('server is running');
+    console.log('listening on port 5000');
 })
-
-//app.get
-//app.post
-//app.put
-//app.delete
-//app.all
-//app.use
-//app.listen
